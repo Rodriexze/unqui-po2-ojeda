76ecc5c95b104bc061de5fb84db6b7d04fdcacbc
Rodriexe <rodrieze2011@hotmail.com>
2025-04-05 00:19:16 -0300

    hastaEj2

--- src/ar/edu/unq/po2/tp3/Counter.java
+++ src/ar/edu/unq/po2/tp3/Counter.java
@@ -2,44 +2,78 @@ package ar.edu.unq.po2.tp3;
 
 import java.util.ArrayList;
 
-public abstract class Counter {
-	
-	private ArrayList <Integer> numeros;
+public class Counter {
+
+	private ArrayList<Integer> numeros;
 
 	public Counter() {
 		this.numeros = new ArrayList<>();
 	}
-	
+
 	public void addNumber(int numero) {
 		numeros.add(numero);
 	}
-	
+
 	public Boolean esPar(int numero) {
-		return numero % 2 == 0 ;
+		return numero % 2 == 0;
 	}
-	
-	public int cantidadDePares(ArrayList <Integer> numeros) {
-		int count = 0 ;
-		for (int numero: numeros) {
+
+	public int cantidadDePares() {
+		int count = 0;
+		for (int numero : numeros) {
 			if (esPar(numero)) {
-				count = + 1;
+				count++;
 			}
 		}
-		return count ;
+		return count;
 	}
-	
+
 	public Boolean esImpar(int numero) {
-		return numero % 2 != 0 ;
+		return numero % 2 != 0;
 	}
-	
-	public int cantidadDeImpares(ArrayList <Integer> numeros) {
-		int count = 0 ;
-		for (int numero: numeros) {
+
+	public int cantidadDeImpares(ArrayList<Integer> numeros) {
+		int count = 0;
+		for (int numero : numeros) {
 			if (esImpar(numero)) {
-				count = + 1;
+				count++;
+			}
 		}
-		return count ;
+		return count;
 	}
+
+//Ej2
 	
-	
+	public int cantidadDePares(int numero) { // no se si funka //TODO cambiar nombre se llama igual q otr arriba
+		int cantidadPares = 0;
+		while (numero > 0) {
+			cantidadPares += this.unoSi(esPar(numero));
+			numero = numero % 10;
+		}
+		return cantidadPares;
+	}
+
+	public int unoSi(boolean condicion) {
+		if (condicion) {
+			return 1;
+		}
+		return 0;
+	}
+
+	public int mayorCantidadDePares() { // separar este lindo plato de spaggetti
+		int numeroConMasParesVisto = 0;
+		int mayorPares = 0;
+		for (int numero : numeros) {
+			if (cantidadDePares(numero) > mayorPares) {
+				mayorPares = cantidadDePares(numero);
+				numeroConMasParesVisto = numero;
+			}
+		}
+		return numeroConMasParesVisto;
+	}
+
+	public int getEvenOcurrences() {
+		return this.cantidadDePares();
+	}
+
 }
--- test/CounterTestCase.java
+++ test/CounterTestCase.java
@@ -1,9 +1,14 @@
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
+import ar.edu.unq.po2.tp3.Counter;
+
 public class CounterTestCase {
-	private counter counter;
+	private Counter counter;
+	
 
 	/**
 	 * Crea un escenario de test básico, que consiste en un contador con 10 enteros
@@ -13,7 +18,8 @@ public class CounterTestCase {
 	@BeforeEach
 	public void setUp() throws Exception {
 //Se crea el contador
-		counter = new counter();
+		counter = new Counter();
+
 //Se agregan los numeros. Un solo par y nueve impares
 		counter.addNumber(1);
 		counter.addNumber(3);
@@ -35,6 +41,6 @@ public class CounterTestCase {
 // Getting the even occurrences
 		int amount = counter.getEvenOcurrences();
 // I check the amount is the expected one
-		assertEquals(amount, 9);
+		assertEquals(amount, 1);
 	}
 }
